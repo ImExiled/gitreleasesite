de80d9ead9faaba8b2a31b1aaec093a87da52bea
CSxKING <csxkingjp@gmail.com>
2018-08-28 17:13:28 -0600

    Initial commit. The very basics and start to everything.

---
+++ assets/js/API.js
@@ -0,0 +1,63 @@
+/* 
+
+    Internal API! DO NOT MODIFY unless you know what you're doing!
+    If you need to add a new function to the system, or change a variable,
+    theres a REALLY good chance its located in "settings.js" in the root of your 
+    site installation and can be changed without modifying this file!
+    
+    YOU HAVE BEEN WARNED!
+
+*/
+
+var API = {
+    getSettings: function() {
+        // We use this function to load our settings.js file
+        $.get('settings.js', function() {
+            API.init();
+        });
+    },
+    internal: {
+        versionChecker: function() {
+            $( document ).ready(function() {
+                $.get(UserSettings.gitLink, function(releases) {
+                    var normal
+                    releases.sort(function(alpha, bravo) {
+                        if( alpha.published_at > bravo.published_at ) return -1;
+                        if( alpha.published_at < bravo.published_at ) return 1;
+                        return 0;
+                    })
+                    .filter(function(rel) {
+                        return !rel.draft && !rel.prerelease
+                    })
+                    .forEach(function(release) {
+                        if( release.target_commitish == 'master' && !normal) normal = release
+                    })
+                    console.info('Normal href-- ' + normal.assets[0].browser_download_url);
+                    console.info('Normal tag_name-- '  + normal.tag_name);
+                    var publishdate = new Date(normal.published_at).toLocaleDateString();
+                    console.info('Normal release_date-- ' + publishdate);
+                });
+            });
+        }
+    },
+    init: function() {
+        if( !UserSettings.advanced.useVersionCheck ) {
+            // If its set to false.
+            console.warn("[INTERNAL] [US] Version checking is disabled!");
+        } else if( UserSettings.advanced.useVersionCheck ) {
+            // If its set to true.
+            if( !UserSettings.advanced.useInternalCheck ) {
+                // If its set to false.
+                UserSettings.advanced.customVersionCheck();
+            } else if( UserSettings.advanced.useInternalCheck ) {
+                // If its set to true.
+                API.internal.versionChecker();
+            }
+        }
+    }
+};
+
+// Self-executing function to load the user settings file, this kick-starts everything.
+(function() {
+    API.getSettings();
+})()
---
+++ settings.js
@@ -0,0 +1,18 @@
+var UserSettings = {
+    // User defined settings that we can load.
+    pageTitle: "Website 123", // The website title.
+    pageHeadTitle: "Website 1234", // The website title that is displayed in the top left.
+    gitLink: "https://api.github.com/repos/dirtdiglett/pressure2/releases", // This is the link that grabs update information.
+    advanced: {
+        // Advanced site things, such as configurable functions and advanced multi-variables.
+        // This is done so that almost nothing has to be edited server-side to add a new feature or
+        // change something.
+        useVersionCheck: true, // Enable or disable the version checker.
+        useInternalCheck: true, // This flag allows the user to use either the default, included version checker or to use their own.
+        customVersionCheck: function() {
+            // This is where the user should write their version checker, as long as the name above
+            // matches. If it doesn't, it'll throw an unrecoverable error.
+            console.warn("IT WORKS!");
+        }
+    }
+};
