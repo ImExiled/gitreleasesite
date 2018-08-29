22f47c0e3e4a229d0ba1e6460bcb7c8aeeef26aa
CSxKING <csxkingjp@gmail.com>
2018-08-29 00:48:28 -0600

    Updated default config links to BandageBD instead.

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 var UserSettings = {
     // User defined settings that we can load.
-    pageTitle: "Pressure2", // The website title.
-    pageHeadTitle: "Pressure<sup>2</sup>", // The website title that is displayed in the top left.
-    downloadBtnTitle: "Download Pressure<sup>2</sup>",
-    gitLink: "https://api.github.com/repos/dirtdiglett/pressure2/releases", // This is the link that grabs update information.
+    pageTitle: "BandagedBetterDiscord", // The website title.
+    pageHeadTitle: "BandagedBD", // The website title that is displayed in the top left.
+    downloadBtnTitle: "Download BandagedBD",
+    gitLink: "https://api.github.com/repos/rauenzi/BetterDiscordApp/releases", // This is the link that grabs update information.
     advanced: {
         // Advanced site things, such as configurable functions and advanced multi-variables.
         // This is done so that almost nothing has to be edited server-side to add a new feature or
